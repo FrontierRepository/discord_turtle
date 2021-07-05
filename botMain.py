ac1cccb6d8c45947144f2163ef8286e160785afc
SKRman6687 <pinray95@gmail.com>
2021-07-05 08:44:04 +0800

    add some notes

--- botMain.py
+++ botMain.py
@@ -10,15 +10,18 @@ bot=commands.Bot(command_prefix="/", intents=intents)
 @bot.event
 async def on_ready():
     print(">>>>>bot is starting to run<<<<<")
+#確認機器人開始運作
 
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(860469237214740491)
     await channel.send(">>"+str(member)+" join!")
+#在成員加入時發送訊息
 
 @bot.event
 async def on_member_remove(member):
     channel = bot.get_channel(860469237214740491)
     await channel.send(">>"+str(member)+" leave!")
+#在成員離開時發送訊息
 
 bot.run(infor["token"])
