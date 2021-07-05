aa035974323c08c99c93250e223ed69d12d752c6
SKRman6687 <pinray95@gmail.com>
2021-07-05 10:35:50 +0800

    add some commands

--- botMain.py
+++ botMain.py
@@ -1,11 +1,11 @@
 import discord
 from discord.ext import commands
 import json
-with open("infor.json", mode="r") as file:
+with open("infor.json", mode="r", encoding="utf8") as file:
     infor=json.load(file)
 
 intents=discord.Intents.all()
-bot=commands.Bot(command_prefix="/", intents=intents)
+bot=commands.Bot(command_prefix="8==D ", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -23,5 +23,14 @@ async def on_member_remove(member):
     channel = bot.get_channel(860469237214740491)
     await channel.send(">>"+str(member)+" leave!")
 #在成員離開時發送訊息
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("what's up sucker")
+#一個打招呼的指令
+
+@bot.command()
+async def drDisrespect(ctx):
+    await ctx.send(infor["drDis"])
 
 bot.run(infor["token"])
