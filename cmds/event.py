7621d9a16582d7b2afd97916e123dd80b4da430c
SKRman6687 <pinray95@gmail.com>
2021-07-08 15:50:46 +0800

    add more rick roll web to avoid rick roll

--- cmds/event.py
+++ cmds/event.py
@@ -35,7 +35,7 @@ class event(cog_extension):
 
     @commands.Cog.listener()
     async def on_message(self, msg):
-        if "https://www.youtube.com/watch?v=dQw4w9WgXcQ" in msg.content or "https://www.youtube.com/watch?v=xvFZjo5PgG0" in msg.content or "https://www.youtube.com/watch?v=QtBDL8EiNZo&t=15s" in msg.content and msg.author!=self.bot.user:
+        if "https://www.youtube.com/watch?v=dQw4w9WgXcQ" in msg.content or "https://www.youtube.com/watch?v=xvFZjo5PgG0" in msg.content or "https://www.youtube.com/watch?v=QtBDL8EiNZo&t=15s" in msg.content or "www.tomorrowtides.com" in msg.content  or "http://www.lasesp.com/article/" in msg.content or "https://rr.noordstar.me" in msg.content and msg.author!=self.bot.user:
           await msg.channel.send("!!!警告!!!這可能是rick roll")
     #在偵測到特定訊息時回復
 
